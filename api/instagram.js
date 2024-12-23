e8d00189e5dfe8eb4793b76181d968c35f7a5eb6
GabrielVitorGL <gabrielvitorcom@hotmail.com>
2024-12-23 03:10:58 -0300

    logging errors

--- api/instagram.js
+++ api/instagram.js
@@ -20,6 +20,7 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(response.status).json(data);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Failed to fetch Instagram profile" });
   }
 }
