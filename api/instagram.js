7fe5871f4c381c8352e58f9f62ed581b405dc181
GabrielVitorGL <gabrielvitorcom@hotmail.com>
2024-12-23 16:53:08 -0300

    changing to instagram private api

--- api/instagram.js
+++ api/instagram.js
@@ -1,26 +1,39 @@
-import fetch from "node-fetch";
+import 'dotenv/config';
+import { IgApiClient } from 'instagram-private-api';
 
 export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
   const { username } = req.query;
 
   if (!username) {
-    return res.status(400).json({ error: "Username is required" });
+    return res.status(400).json({ error: 'Username query parameter is required' });
   }
 
-  const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`;
-  const headers = {
-    "User-Agent":
-      "Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743)",
-    Origin: "https://www.instagram.com",
-    Referer: "https://www.instagram.com/",
-  };
+  const ig = new IgApiClient();
+
+  ig.state.generateDevice(process.env.IG_USERNAME);
+  ig.state.proxyUrl = process.env.IG_PROXY || null;
 
   try {
-    const response = await fetch(url, { headers });
-    const data = await response.json();
-    res.status(response.status).json(data);
+    await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+
+    const userId = await ig.user.getIdByUsername(username);
+
+    const userInfo = await ig.user.info(userId);
+
+    const followersCount = userInfo.follower_count;
+
+    console.log(followersCount);
+    return res.status(200).json({
+      username: userInfo.username,
+      fullName: userInfo.full_name,
+      followers: followersCount,
+    });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to fetch Instagram profile" });
+    console.error('Erro ao consultar perfil:', error.message);
+    return res.status(500).json({ error: 'Failed to fetch user info', details: error.message });
   }
 }
